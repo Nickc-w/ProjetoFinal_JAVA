a30ad7c2cb64bfe20d617f3a3fa6ad8238aaa10d
Nicolly Candida <nicolly.candida2005@gmail.com>
2024-11-11 13:25:40 -0300

    Update LembreteRepository.java

--- MeuProjeto/src/main/java/com/ProjetoJava/MeuProjeto/Interface/LembreteRepository.java
+++ MeuProjeto/src/main/java/com/ProjetoJava/MeuProjeto/Interface/LembreteRepository.java
@@ -7,13 +7,7 @@ import com.ProjetoJava.MeuProjeto.models.Lembrete;
 
 import java.util.List;
 
-// <Entidade: Define qual tipo de objeto (classe) o repositório vai manipular, tipo do id ( pq mts metodos usam o id)
 public interface LembreteRepository extends CrudRepository<Lembrete, Long> {
-    //LembreteRepository (fala "repositorio) pq..., estamos dizendo que
-    // ele vai "armazenar" os métodos que lidam com a manipulação de dados da
-    // entidade Lembrete no banco de dados.
-
     List<Lembrete> findByTituloContaining(String titulo);
-    // porque nao tem um metodo especifico de busca pelo TITULO!! Entao usa esse que o O Spring Data JPA oferece
-
+    // porque nao tem um metodo especifico de busca pelo TITULO!! Entao usa esse que o Spring Data JPA oferece
 }
